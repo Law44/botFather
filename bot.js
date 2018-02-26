6e10a67161b6c6a2e9375e02227f4efa8b5c3381
Law44 <36800363+Law44@users.noreply.github.com>
2018-02-26 12:56:38 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,9 +7,13 @@ var quotes =
 "Una lección sin sufrimiento no tiene sentido. Eso es porque no puedes obtener algo sin sacrificar otra cosa a cambio. Pero una vez que has soportado el dolor y lo has superado, obtendrás un corazón que es más fuerte que cualquier cosa. Sí, un corazón de acero.", 
 "¿Los piratas son malos? ¿Los marines son los buenos? ¡Estos términos han cambiado siempre a lo largo de la historia! ¡Los niños que nunca han visto la paz y los niños que nunca han visto la guerra tienen valores diferentes! ¡Los que están en la cima determinan lo que está bien y lo que está mal! ¡Este lugar es un terreno neutral! ¿Dicen que la Justicia prevalecerá? ¡Por supuesto que lo hará! ¡Gane quién gane esta guerra se convertirá en la Justicia!", 
 "No confundas mi personalidad con mi actitud. Mi personalidad es quien soy yo, mi actitud depende de quien seas tú."
+"Cree en ti mismo. No en el mí que cree en ti. Ni en el tú que cree en mi. ¡Cree en el tú... que cree en ti mismo!"
 ]
 var prefix = '!';
-bot.on('message', message => {		
+bot.on('message', message => {
+		if (message.content === prefix + 'help') {
+				message.channel.send('!Alberto\n!Adri\n!DesertFox\n!quote');
+			}
 		if (message.content === prefix + 'Alberto') {
 				message.reply('Esteeeeeee Meeeeeeeeen');
 			}
@@ -20,7 +24,7 @@ bot.on('message', message => {
 				message.reply('BOMBA DE HUMO! BOMBA DE HUMO!');
 			}
 		if (message.content === prefix + 'quote') {
-				message.channel.send(quotes[Math.floor(Math.random() * 4)]);
+				message.channel.send(quotes[Math.floor(Math.random() * 6)]);
 			}
 });
 bot.login(process.env.BOT_TOKEN);
